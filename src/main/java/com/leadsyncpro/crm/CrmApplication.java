4760f5f8b56b70d06a1bfe04249830ae61fc6242
Mehdi Erdem DEDE <mehdierdemdede@gmail.com>
2025-11-21 08:51:06 +0300

    Fix CrmApplication class visibility and configure main class

--- src/main/java/com/leadsyncpro/crm/CrmApplication.java
+++ src/main/java/com/leadsyncpro/crm/CrmApplication.java
@@ -1,22 +1,20 @@
-// src/main/java/com/leadsyncpro/crm/CrmApplication.java
 package com.leadsyncpro.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan; // Yeni import
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.leadsyncpro") // Tüm ana paketi tara
-@EnableJpaRepositories(basePackages = "com.leadsyncpro.repository") // Repository'leri tara
-@EntityScan(basePackages = "com.leadsyncpro.model") // BURASI EKLENDİ: Entity'leri tara
+@ComponentScan(basePackages = "com.leadsyncpro")
+@EnableJpaRepositories(basePackages = "com.leadsyncpro.repository")
+@EntityScan(basePackages = "com.leadsyncpro.model")
 @EnableScheduling
 public class CrmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrmApplication.class, args);
-	}
-
+        public static void main(String[] args) {
+                SpringApplication.run(CrmApplication.class, args);
+        }
 }
